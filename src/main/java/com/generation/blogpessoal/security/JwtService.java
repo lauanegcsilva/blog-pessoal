2900cda9cd6c5a841f73ae987940979d0f1291ea
Lauane Gonçalves <lauane.gcsilva@gmail.com>
2024-01-29 15:09:19 -0300

    fix: alterachave secreta

--- src/main/java/com/generation/blogpessoal/security/JwtService.java
+++ src/main/java/com/generation/blogpessoal/security/JwtService.java
@@ -18,7 +18,7 @@ import io.jsonwebtoken.security.Keys;
 @Component
 public class JwtService {
 
-	public static final String SECRET = "5367566B59703373367639792F423F4528482B4D6251655468576D5A71347437";
+	public static final String SECRET = "46be0927a4f86577f17ce6d10bc6aa61";
 
 	private Key getSignKey() {
 		byte[] keyBytes = Decoders.BASE64.decode(SECRET);
